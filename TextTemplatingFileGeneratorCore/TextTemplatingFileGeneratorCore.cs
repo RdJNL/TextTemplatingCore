92d83f2152939df681674420dbe3eac9b7d587df
Marian Mitschke <m.mitschke@raynet.de>
2023-12-07 15:20:30 +0100

    dotnet 8 support added

--- TextTemplatingFileGeneratorCore/TextTemplatingFileGeneratorCore.cs
+++ TextTemplatingFileGeneratorCore/TextTemplatingFileGeneratorCore.cs
@@ -23,7 +23,7 @@ namespace RdJNL.TextTemplatingCore.TextTemplatingFileGeneratorCore
     {
         public const string GENERATOR_GUID = "85B769DE-38F5-4CBE-91AE-D0DFA431FE30";
         public const string GENERATOR_NAME = nameof(TextTemplatingFileGeneratorCore);
-        public const string GENERATOR_DESCRIPTION = "Generate files from T4 templates using the .NET 7 runtime.";
+        public const string GENERATOR_DESCRIPTION = "Generate files from T4 templates using the .NET 8 runtime.";
 
         private const string ERROR_OUTPUT = "ErrorGeneratingOutput";
         private const string TEMPLATE_NAMESPACE = "RdJNL.TextTemplatingCore.GeneratedTemplate";
